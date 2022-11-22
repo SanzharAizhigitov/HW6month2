2a3df89d28247d0c8f3bad67e5634f088ff846af
SanzharAizhigitov <ajzigitovsanzar@gmail.com>
2022-11-22 19:50:31 +0600

    Initial commit

---
+++ app/src/main/java/com/geektech/hw6/MainActivity.java
@@ -0,0 +1,54 @@
+package com.geektech.hw6;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EditText poshta = findViewById(R.id.poshta);
+        EditText parol = findViewById(R.id.parol);
+        Button button = findViewById(R.id.vxod);
+        EditText editText2 = findViewById(R.id.parol);
+        EditText editText1 = findViewById(R.id.poshta);
+        TextView textView1 = findViewById(R.id.vxod);
+        TextView textView2 = findViewById(R.id.Dobro);
+        TextView textView3 = findViewById(R.id.Vipolnite);
+        TextView textView4 = findViewById(R.id.zabil1);
+        TextView textView5 = findViewById(R.id.zabil2);
+
+        setContentView(R.layout.activity_main);
+        button.setBackgroundColor(ContextCompat.getColor(MainActivity.this,R.color.orange));
+        findViewById(R.id.vxod).setOnClickListener(view -> {
+            if (poshta.getText().toString().equals("admin")&& parol.getText().toString().equals("admin")){
+                editText1.setVisibility(View.GONE);
+                editText2.setVisibility(View.GONE);
+                textView1.setVisibility(View.GONE);
+                textView3.setVisibility(View.GONE);
+                textView4.setVisibility(View.GONE);
+                textView5.setVisibility(View.GONE);
+                button.setVisibility(View.GONE);
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "Вы успешно зарегистрировались", Toast.LENGTH_SHORT);
+                toast.show();
+
+            }else {
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "Неправильный логин и пароль", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+
+               }
+                );
+
+    }
+}
